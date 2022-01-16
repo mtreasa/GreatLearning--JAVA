f0ce7335d03205b0e5a375457268de08cd7bb682
Treasa <maliakal.treasa@gmail.com>
2022-01-16 22:31:50 +0530

    commit the hello world file to GIT

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+	public static void main(String[] args){
+	System.out.println("Hello World");
+	int _var=5;
+	System.out.println(_var);
+	int Int=1;
+		System.out.println(Int);
+    }
+}
